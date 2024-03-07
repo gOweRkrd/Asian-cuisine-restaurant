ff35f2e2026afe883dd5c5b514459fb98fb4c18d
Alexander Kosyakov <110721351+gOweRkrd@users.noreply.github.com>
2024-03-07 10:46:55 +0300

    Delete storyBoard, add custom font and colors, add strings , add SwiftLint

---
+++ Asian-cuisine-restaurant/Resources/R.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+enum StringResources {
+    
+    enum ViewController {
+        static let title = "Заголовок"
+    }
+        
+    enum Colors {
+        static let white = UIColor(hexString: "#FFFFFF")
+        static let black = UIColor(hexString: "##313131")
+        static let gray = UIColor(hexString: "#96959B")
+        static let lightGray = UIColor(hexString: "#F3F3F5")
+    }
+}
+
+typealias R = StringResources
